be36bb3fdbc552e753e1b83cd35192bb043a1b50
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-09 18:12:41 -0500

    added dot to messages link

--- client/src/components/AuthMenu/useStyles.ts
+++ client/src/components/AuthMenu/useStyles.ts
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    marginLeft: 20,
+  },
   smallScreenItem: {
     [theme.breakpoints.up('md')]: {
       display: 'none',
