902421f0d6e2a95dc3d63daf2ff4bc49c38d6df1
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-12 11:40:20 -0500

    updated template pages

--- client/src/pages/Dashboard/profile/editProfile/EditProfile.tsx
+++ client/src/pages/Dashboard/profile/editProfile/EditProfile.tsx
@@ -1,20 +1,6 @@
 import Grid from '@material-ui/core/Grid';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { useAuth } from '../../../../context/useAuthContext';
-import { useHistory } from 'react-router-dom';
 
 export default function EditProfile(): JSX.Element {
-  const { loggedInUser } = useAuth();
-
-  const history = useHistory();
-
-  if (loggedInUser === undefined) return <CircularProgress />;
-  if (!loggedInUser) {
-    history.push('/login');
-    // loading for a split seconds until history.push works
-    return <CircularProgress />;
-  }
-
   return (
     <Grid container component="main">
       <Grid item>
