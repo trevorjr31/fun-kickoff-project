6f7a8de433b1fde98dda00058c1a7b6a4a4edd5d
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-14 18:42:01 -0500

    updated layout components

--- client/src/pages/Dashboard/profile/editProfile/EditProfile.tsx
+++ client/src/pages/Dashboard/profile/editProfile/EditProfile.tsx
@@ -1,10 +1,11 @@
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 export default function EditProfile(): JSX.Element {
   return (
     <Grid container component="main">
       <Grid item>
-        <h2>edit profile</h2>
+        <Typography>edit profile</Typography>
       </Grid>
     </Grid>
   );
--- client/src/pages/Dashboard/profile/profilePhoto/ProfilePhoto.tsx
+++ client/src/pages/Dashboard/profile/profilePhoto/ProfilePhoto.tsx
@@ -1,10 +1,11 @@
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 export default function ProfilePhoto(): JSX.Element {
   return (
     <Grid container component="main">
       <Grid item>
-        <h2>profile photo</h2>
+        <Typography>profile photo</Typography>
       </Grid>
     </Grid>
   );
