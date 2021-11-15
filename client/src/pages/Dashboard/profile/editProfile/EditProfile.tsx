51c43c136f3844588747644e01999a0d235cb4b9
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-15 12:52:48 -0500

    implemented profile form ui

--- client/src/pages/Dashboard/profile/editProfile/EditProfile.tsx
+++ client/src/pages/Dashboard/profile/editProfile/EditProfile.tsx
@@ -1,39 +1,18 @@
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid, Box } from '@material-ui/core';
 import useStyles from './useStyles';
-import { TextField } from '@material-ui/core';
+import EditProfileForm from './editProfileForm/EditProfileForm';
 
 export default function EditProfile(): JSX.Element {
   const classes = useStyles();
+
   return (
     <Grid container justify="center" component="main">
       <Grid item>
-        <Box pt={7}>
-          <Typography className={classes.title}>edit profile</Typography>
-          <form>
-            <Box display="flex" justifyContent="space-around" minWidth={350}>
-              <Typography className={classes.label}>First Name</Typography>{' '}
-              <TextField
-                className={classes.field}
-                variant="outlined"
-                placeholder="Your email"
-                id="email"
-                margin="normal"
-                InputLabelProps={{
-                  disableAnimation: false,
-                }}
-                InputProps={{
-                  classes: { input: classes.inputs },
-                }}
-                name="email"
-                autoComplete="email"
-                autoFocus
-                helperText={'g'}
-                value={'values.email'}
-              />
-            </Box>
-          </form>
+        <Box pt={5}>
+          <Box display="flex" justifyContent="center">
+            <Typography className={classes.title}>edit profile</Typography>
+          </Box>
+          <EditProfileForm />
         </Box>
       </Grid>
     </Grid>
--- client/src/pages/Dashboard/profile/editProfile/useStyles.ts
+++ client/src/pages/Dashboard/profile/editProfile/useStyles.ts
@@ -1,24 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { theme } from '../../../../themes/theme';
 
 const useStyles = makeStyles(() => ({
   title: {
     fontSize: 24,
     textTransform: 'capitalize',
     fontWeight: 600,
-  },
-  inputs: {
-    marginLeft: theme.spacing(1),
-    height: '2rem',
-    padding: '5px',
-  },
-  label: {
-    marginTop: theme.spacing(3.5),
-  },
-  field: {
-    width: '25vw',
-    minWidth: '200px',
-    maxWidth: '300px',
+    alignSelf: 'center',
   },
 }));
 
