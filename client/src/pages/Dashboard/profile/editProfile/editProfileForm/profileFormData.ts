0d96cae60d6ded4d955d51ba766f07dda7503ddf
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-16 11:17:18 -0500

    removed some notes

--- client/src/pages/Dashboard/profile/editProfile/editProfileForm/profileFormData.ts
+++ client/src/pages/Dashboard/profile/editProfile/editProfileForm/profileFormData.ts
@@ -16,17 +16,17 @@ const profileFormData = {
     'November',
     'December',
   ],
-
   years,
   dates,
 };
-//create a list of numbers starting at 1900, going untill the current year
+
 const currentYear = new Date().getFullYear();
+
 for (let i = 1900; i < currentYear; i++) {
   const str = i.toString();
   years.push(str);
 }
-//create list for dates
+
 for (let i = 0; i < 32; i++) {
   const str = i.toString();
   dates.push(str);
